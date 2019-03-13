7b7993e14922cca490cc144f3db86449ed634070
mrhyde <mr.hyde@2bad.me>
2019-03-13 20:35:34 +0300

    [ts] add types for function parameters and return values

--- source/types/mango.ts
+++ source/types/mango.ts
@@ -1,18 +1,14 @@
-export interface AuthOptions {
+export interface AuthParams {
   readonly username: string
   readonly password: string
   readonly app: string
   readonly device_id: string
 }
 
-export interface Options {
-  readonly authToken: string
-  readonly refreshToken: string
-  readonly accountId: number
-  readonly productId: number
-  readonly customerId: number
-  readonly abonentId: number
-  readonly pointId: string
+export interface CreateSessionParams {
+  readonly auth_token: string
+  readonly refresh_token: string
+  readonly username: string
   readonly app: string
 }
 
@@ -29,6 +25,50 @@ export interface AuthResponse {
   readonly authMethod: string
 }
 
+export enum StatCallsPeriod {
+  arbitrary = 'arbitrary',
+  today2 = 'today2',
+  yesterday = 'yesterday',
+  current_week = 'current_week',
+  current_month = 'current_month',
+  last_month = 'last_month',
+  current_quarter = 'current_quarter',
+  last_quarter = 'last_quarter'
+}
+
+export enum CallerType {
+  member = 'member',
+  client = 'client',
+  client_or_member = 'client_or_member'
+}
+
+export enum CallStatus {
+  all = 'all',
+  success = 'success',
+  fail = 'fail'
+}
+
+export type DateString = string
+
+export interface StatCallsParams {
+  readonly perpage: number
+  readonly type: 'calls'
+  readonly period: StatCallsPeriod
+  readonly startDate: DateString
+  readonly endDate: DateString
+  readonly whoCalledType: CallerType
+  readonly whoCalled: string
+  readonly clientNumber: number | undefined
+  readonly line: number | 'any'
+  readonly groups: ReadonlyArray<string>
+  readonly phones: ReadonlyArray<string>
+  readonly offset: number
+  readonly sorting: string | undefined
+  readonly fromLine: number | undefined
+  readonly members: ReadonlyArray<string>
+  readonly callStatus: CallStatus
+  readonly filterParty: 'listall'
+}
 export interface StatsResponse {
   readonly status: boolean
 }
