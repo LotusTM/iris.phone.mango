b94ba937d8fc047bdf04c384c526557afebf1141
mrhyde <mr.hyde@2bad.me>
2019-03-14 01:05:55 +0300

    [ts] specify exact imports from auth module

--- source/mango.ts
+++ source/mango.ts
@@ -1,14 +1,14 @@
 import got from 'got'
 import { name, version } from './../package.json'
-import * as auth from './auth'
+import { createSession, login } from './auth'
 
 import * as Mango from './types/mango'
 
 const REST_URL = `https://lk.mango-office.ru`
 
 export const create = async (options?: Mango.AuthParams) => {
-  const authData = await auth.login(options)
-  const { request } = await auth.createSession(authData)
+  const authData = await login(options)
+  const { request } = await createSession(authData)
   return got.extend({
     baseUrl: `${REST_URL}/${authData.account_id}/${authData.product_id}`,
     cookieJar: request.gotOptions.cookieJar,
