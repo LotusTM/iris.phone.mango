865a788043822e31fb5d4bb5bb30dab87ebf9124
mrhyde <mr.hyde@2bad.me>
2019-03-18 17:49:14 +0300

    [types] add `Iris.Call` type

---
+++ source/iris/types/iris.ts
@@ -0,0 +1,29 @@
+export interface Call {
+  readonly date: string
+  readonly direction: string
+  readonly caller: string
+  readonly line: string
+  readonly member: ReadonlyArray<Member>
+  readonly duration: string
+  readonly callback_id: number
+  readonly cost: number
+}
+
+export interface Member {
+  readonly type: string
+  readonly id: number
+  readonly created_at: string
+  readonly answered_at: string | null
+  readonly end_at: string
+  readonly end_reason: string
+  readonly duration: Duration
+  readonly intercepted: boolean
+  readonly answered: boolean
+}
+
+export interface Duration {
+  readonly hold: number
+  readonly call: number
+  readonly talk: number
+  readonly dial: number
+}
